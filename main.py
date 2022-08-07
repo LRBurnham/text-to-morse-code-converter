d2be28c7929210f738df7cfd1188db21c9e1175d
LR Burnham <lilyroocodes@gmail.com>
2022-08-07 01:22:39 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from morse_code import code
+
+print("Enter STOP to end the program")
+
+while True:
+    user_text = input("What would you like to be translated to morse code? \n").upper()
+    if user_text == "STOP":
+        break
+
+    split_text = [char for char in user_text]
+    morse_text = ""
+
+    for character in split_text:
+        morse_text += f"{code[character]} "
+
+    print(morse_text)
+
+
